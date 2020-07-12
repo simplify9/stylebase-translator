9bedfe01761eea06c7ef291f9a1cf90ff12979ec
shaheen-s <shaheensarafa17@gmail.com>
2020-07-12 16:11:32 +0300

    blazorProject to keep track of documents

--- include/blazorTypes/BlazorProject.hpp
+++ include/blazorTypes/BlazorProject.hpp
@@ -7,6 +7,7 @@
 class BlazorProject {
     public:
         static std::unordered_map<int, BlazorComponent*> componentIds;
+        static std::vector<BlazorDocument> documents;
         BlazorProject();
     private:
 };
--- src/blazorTypes/BlazorProject.cpp
+++ src/blazorTypes/BlazorProject.cpp
@@ -1,5 +1,6 @@
 #include <blazorTypes/BlazorProject.hpp>
 
 std::unordered_map<int, BlazorComponent*> BlazorProject::componentIds;
+std::vector<BlazorDocument> BlazorProject::documents;
 
 BlazorProject::BlazorProject() {}
