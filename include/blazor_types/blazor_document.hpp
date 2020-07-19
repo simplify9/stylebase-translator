ef60829f8de05460cc0350e400c26eae57e9dcd5
shaheen-s <shaheensarafa17@gmail.com>
2020-07-19 15:37:06 +0300

    Adding streaming to blazor document

--- include/blazor_types/blazor_document.hpp
+++ include/blazor_types/blazor_document.hpp
@@ -9,10 +9,15 @@ class BlazorDocument {
         BlazorDocument(const char* name);
         BlazorDocument(BlazorComponent mainComp);
         BlazorDocument(const char* name, BlazorComponent mainComp);
+
+        void writeToFile();
+
         inline std::string getName() {return name;};
         void setName(const char* name);
     private:
         std::string name;
+        std::vector<std::string> usings;
+        std::string csharp_namespace;
         BlazorComponent mainComponent;
 };
 
--- src/blazor_types/blazor_document.cpp
+++ src/blazor_types/blazor_document.cpp
@@ -1,4 +1,9 @@
 #include <blazor_types/blazor_document.hpp>
+#include <constants/stylebase_consts.hpp>
+
+#include <fstream>
+#include <filesystem>
+#include <blazor_types/blazor_project.hpp>
 
 BlazorDocument::BlazorDocument() {}
 
@@ -11,8 +16,27 @@ BlazorDocument::BlazorDocument(const char* name, BlazorComponent comp):
 {}
 
 BlazorDocument::BlazorDocument(BlazorComponent comp):
-    name(comp.getName()), mainComponent(comp)
-{}
+    name(comp.getName() + ".razor"), mainComponent(comp), csharp_namespace("Stylebase.Components")
+{
+    for(std::string lib : comp.getLibraries()){
+        this->usings.emplace_back(lib);
+    }
+}
+
+void BlazorDocument::writeToFile(){
+    const std::string ns = "@namespace " + this->csharp_namespace;
+    std::string usingStatements = "";
+
+    for(std::string lib : mainComponent.getLibraries())
+        usingStatements += "\n@using " + STYLEBASE_LIB + '.' + lib;
+
+    std::ofstream file;
+    if(BlazorProject::specifiedDir){
+    }
+    file << ns << usingStatements << std::endl;
+    mainComponent.streamOutput<std::ofstream*>(&file);
+    file.close();
+}
 
 
 void BlazorDocument::setName(const char* name) {this->name = name;}
